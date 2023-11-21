b626245df6a3cd7d46012b37e878b750bc30444f
annemarielewis <a.m.lewisfilms@gmail.com>
2023-11-21 10:49:05 -0700

    links

--- nov-hackathon-vite/src/components/Main.jsx
+++ nov-hackathon-vite/src/components/Main.jsx
@@ -1,17 +1,20 @@
 // import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Channel from './Channels';
+import Channel from "./Channels";
 // import Filter from './Filter';
-import Navbar from './Navbar';
-
+import Navbar from "./Navbar";
+import Outcomes from "./Outcomes";
+import SEI from "./SEI";
 
 const Main = () => {
   return (
     <div className="main-container">
       <Navbar />
       <Routes>
-        <Route path='/channel' element={<Channel />} />
+        <Route path="/channel" element={<Channel />} />
         {/* <Route path='/filter' element={<Filter />} /> */}
+        <Route path="/outcomes" element={<Outcomes />} />
+        <Route path="/SEI" element={<SEI />} />
       </Routes>
     </div>
   );
--- nov-hackathon-vite/src/components/Navbar.jsx
+++ nov-hackathon-vite/src/components/Navbar.jsx
@@ -5,9 +5,15 @@ const Navbar = () => {
     <div className="navbar-area">
       <h1 className="dashboard">Dashboard</h1>
       <div className="navbar-container">
-        <div className="nav-items">Outcomes</div>
-        <div className="nav-items">UXDI</div>
-        <div className="nav-items">Online Communities</div>
+        <Link to="/outcomes">
+          <div className="nav-items">Outcomes</div>
+        </Link>
+        <Link to="/SEI">
+          <div className="nav-items">SEI</div>
+        </Link>
+        <Link to="/channel">
+          <div className="nav-items">Online Communities</div>
+        </Link>
       </div>
     </div>
   );
