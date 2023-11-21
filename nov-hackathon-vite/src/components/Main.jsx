ef0be475784a56965c1405682cbcea2fb06bcae6
darcystruble <darcyhallstruble@gmail.com>
2023-11-20 20:09:13 -0600

    main has basic routes for viewing compenents

--- nov-hackathon-vite/src/components/Main.jsx
+++ nov-hackathon-vite/src/components/Main.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Channel from './Channels'
+import Filter from './Filter'
+import Header from './Header'
+import Navbar from './Navbar'
+import App from '../App'
 
 const Main = () => {
-  return <div></div>;
+  return <div>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/channel' element={<Channel />} />
+      <Route path='/filter' element={<Filter />} />
+      <Route path='/header' element={<Header />} />
+      <Route path='/navbar' element={<Navbar />} />
+    </Routes>
+  </div>;
 };
 
 export default Main;
