068afc7180be8d65120d0e0cf9ae176988ee9f75
BWhittaker1415 <bwhittaker1415@gmail.com>
2023-11-20 20:29:19 -0500

    boiler plates for components

--- nov-hackathon-vite/src/App.jsx
+++ nov-hackathon-vite/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div>
-    </div>
-  )
+    <>
+      <div>
+        {/* navbar might not be functional */}
+        <Navbar />
+        <Header />
+
+        {/* for all routes */}
+        <Main.jsx />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
