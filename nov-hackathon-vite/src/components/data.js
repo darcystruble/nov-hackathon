fd0f9615a9b6ac8c7726a3a3c727f640c50935ff
annemarielewis <a.m.lewisfilms@gmail.com>
2023-11-20 20:39:40 -0700

    all data added

---
+++ nov-hackathon-vite/src/components/data.js
@@ -0,0 +1,123 @@
+const channels = [
+  {
+    id: 1,
+    name: "ga-lgbtqia-srg",
+    description:
+      "A student resource group uniting members of the LGBTQIA+ community & Allies",
+    image: "../assets/lgbtqia.png",
+    type: "identity",
+    timezone: "N/A",
+    course: "All",
+  },
+  {
+    id: 2,
+    name: "ga-pets-srg",
+    description:
+      "A student resource group uniting all pet lovers. Whether furry, feathered, scaled or imaginary - all are welcome!",
+    image: "../assets/pets.png",
+    type: "fun",
+    timezone: "N/A",
+    course: "All",
+  },
+
+  {
+    id: 3,
+    name: "ga-epic-srg",
+    description:
+      "EPIC stands for Empowered People and Inclusive Cultures. A student resource group uniting our diverse & unique community. Celebrating dimensions of race, ethnicity, gender, sexual orientation, socio-economic status, age, physical abilities, religious beliefs, political beliefs & ideologies",
+    image: "../assets/empowered.png",
+    type: "identity",
+    timezone: "N/A",
+    course: "All",
+  },
+  {
+    id: 4,
+    name: "ga-womenintech-srg",
+    description:
+      "A student resource group uniting those who self-identify as a woman, transgender woman, genderqueer or non-binary",
+    image: "../assets/women.png",
+    type: "identity",
+    timezone: "N/A",
+    course: "All",
+  },
+  {
+    id: 5,
+    name: "ga-parents-guardians-srg",
+    description:
+      "A student resource group uniting guardians such as experienced, new, or expecting parents, or those caring for elderly, sick or disabled relatives.",
+    image: "../assets/parents.png",
+    type: "identity",
+    timezone: "N/A",
+    course: "All",
+  },
+  {
+    id: 6,
+    name: "ga-neurodiversityintech-srg",
+    description:
+      "Championing Neurodiversity at GA & creating an inclusive community for minds of all kinds.",
+    image: "../assets/neurodiversity.png",
+    type: "identity",
+    timezone: "N/A",
+    course: "All",
+  },
+  {
+    id: 7,
+    name: "ga-gamers-srg",
+    description:
+      "A gathering space for fans of Console, Table Top, RPG, MMORPG, Classic, Mobile, and all OTHER types of games.",
+    image: "../assets/gamers.png",
+    type: "fun",
+    timezone: "N/A",
+    course: "All",
+  },
+  {
+    id: 8,
+    name: "ga-blackintech-srg",
+    description:
+      "A Student Resource Group to unite and support Black members of the GA student community.",
+    image: "../assets/blackintech.png",
+    type: "identity",
+    timezone: "N/A",
+    course: "All",
+  },
+  {
+    id: 9,
+    name: "uxdi-lounge",
+    description:
+      "Space for students from all different cohorts of UXDI to socialize and network!",
+    image: "../assets/uxdi.png",
+    type: "network",
+    timezone: "N/A",
+    course: "UXDI",
+  },
+  {
+    id: 10,
+    name: "sei-lounge",
+    description:
+      "Space for students from all different cohorts of SEI to socialize and network!",
+    image: "../assets/sei.png",
+    type: "network",
+    timezone: "N/A",
+    course: "SEI",
+  },
+  {
+    id: 11,
+    name: "dsi-lounge",
+    description:
+      "Space for students from all different cohorts of DSI to socialize and network!",
+    image: "../assets/dsi.png",
+    type: "network",
+    timezone: "N/A",
+    course: "DSI",
+  },
+  {
+    id: 12,
+    name: "hired-in-tech",
+    description:
+      "Space for students from all classes and cohorts to post when they get hired, detailing their experience/achievement",
+    image: "../assets/hiring.png",
+    type: "network",
+    timezone: "N/A",
+    course: "ALL",
+  },
+];
